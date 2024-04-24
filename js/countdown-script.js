4f3a310ad3d485e5e64776fe143d6d8c58f14f64
BeepBot99 <74377508+BeepBot99@users.noreply.github.com>
2024-04-24 09:12:33 -0500

    Added alert

--- js/countdown-script.js
+++ js/countdown-script.js
@@ -39,6 +39,9 @@ $("#copy-button").on("click", function () {
 });
 
 $(".icon-select-button").on("click", function () {
+    alert($(this)
+    .find("i.material-symbols-outlined")
+    .text());
     $("#page-icon-input").val(
         $(this)
             .find("i.material-symbols-outlined")
