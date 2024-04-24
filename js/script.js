6ea2b5f7867569109fc129d2a432f651df36deb4
BeepBot99 <74377508+BeepBot99@users.noreply.github.com>
2024-04-24 10:22:27 -0500

    changed some stuff

--- js/script.js
+++ js/script.js
@@ -1,112 +1,73 @@
-// let params = new URL(document.location.toString()).searchParams;
+const params = new URL(document.location.toString()).searchParams;
+if (!params.get("endTime")) location.replace("./generator");
 
-// let caption = params.get('caption');
-// let showcaption = params.get('showcaption');
-// let endtime = params.get('endtime');
-// let endmessage = params.get('endmessage');
-// let icon = params.get('icon');
-// let title = params.get('title');
+const fillers = {
+    caption: params.get("captionFull") || "Countdown",
+    hideCaption: params.get("hidecaption") || false,
+    endTime: params.get("endDate"),
+    endMessage: params.get("endMessage") || "The countdown has ended.",
+    icon: params.get("pageIcon") || "timer",
+    title: params.get("pageTitle") || "Countdown"
+}
 
-// let changed = false;
+$("#heading, #heading-mobile").text(fillers.caption);
 
-// if (!showcaption) {
-//     showcaption = 'true';
-//     changed = true;
-// }
+const countdownElement = document.getElementById('element');
 
-// if (!caption) {
-//     caption = 'undefined';
-//     showcaption = 'false';
-//     changed = true;
-// }
+if (showcaption == 'false') {
+    document.getElementById('caption').style.display = 'none';
+}
 
-// if (!endmessage) {
-//     endmessage = 'Time reached!'
-//     changed = true;
-// }
+document.title = title;
 
-// if (!icon) {
-//     icon = 'https://projects.mesure.x10.mx/countdown/favicon.png';
-//     changed = true;
-// }
+var link = document.querySelector("link[rel~='icon']");
+if (!link) {
+    link = document.createElement('link');
+    link.rel = 'icon';
+    document.head.appendChild(link);
+}
+link.href = icon;
 
-// if (!title) {
-//     title = `Countdown until ${countdown}`;
-//     changed = true;
-// }
+var countDownDate = new Date(parseInt(endtime)).getTime();
 
-// if (!endtime) {
-//     caption = 'the New Year';
-//     showcaption = 'true';
-//     endtime = '1735689600000';
-//     endmessage = 'It is now the Jan 1 in GMT!';
-//     icon = 'https://em-content.zobj.net/source/skype/289/party-popper_1f389.png';
-//     title = 'New Year Countdown';
-//     changed = true;
-// }
+// Update the count down every 1 second
+var x = setInterval(function () {
 
-// if (changed) {
-//     window.location.href = `https://projects.mesure.x10.mx/countdown/?caption=${caption}&showcaption=${showcaption}&endtime=${endtime}&endmessage=${endmessage}&icon=${icon}&title=${title}`;
-// }
+    // Get today's date and time
+    var now = new Date().getTime();
 
-// document.getElementById('caption').innerText = `Countdown until ${caption}`;
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
 
-// const countdownElement = document.getElementById('element');
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-// if (showcaption == 'false') {
-//     document.getElementById('caption').style.display = 'none';
-// }
+    if (days < 10) {
+        days = `0${days}`;
+    }
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    if (seconds < 10) {
+        seconds = `0${seconds}`;
+    }
 
-// document.title = title;
+    // Display the result in the element with id="demo"
+    countdownElement.innerHTML = `<span class="amount">${days}</span><span class="unit">d</span> <span class="amount">${hours}</span><span class="unit">h</span> <span class="amount">${minutes}</span><span class="unit">m</span> <span class="amount">${seconds}</span><span class="unit">s</span>`
 
-// var link = document.querySelector("link[rel~='icon']");
-// if (!link) {
-//     link = document.createElement('link');
-//     link.rel = 'icon';
-//     document.head.appendChild(link);
-// }
-// link.href = icon;
+    // If the count down is finished, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        countdownElement.innerHTML = endmessage;
+    }
+}, 1000);
 
-// var countDownDate = new Date(parseInt(endtime)).getTime();
-
-// // Update the count down every 1 second
-// var x = setInterval(function () {
-
-//     // Get today's date and time
-//     var now = new Date().getTime();
-
-//     // Find the distance between now and the count down date
-//     var distance = countDownDate - now;
-
-//     // Time calculations for days, hours, minutes and seconds
-//     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-//     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-//     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-//     if (days < 10) {
-//         days = `0${days}`;
-//     }
-//     if (hours < 10) {
-//         hours = `0${hours}`;
-//     }
-//     if (minutes < 10) {
-//         minutes = `0${minutes}`;
-//     }
-//     if (seconds < 10) {
-//         seconds = `0${seconds}`;
-//     }
-
-//     // Display the result in the element with id="demo"
-//     countdownElement.innerHTML = `<span class="amount">${days}</span><span class="unit">d</span> <span class="amount">${hours}</span><span class="unit">h</span> <span class="amount">${minutes}</span><span class="unit">m</span> <span class="amount">${seconds}</span><span class="unit">s</span>`
-
-//     // If the count down is finished, write some text
-//     if (distance < 0) {
-//         clearInterval(x);
-//         countdownElement.innerHTML = endmessage;
-//     }
-// }, 1000);
-
-// document.getElementById('settings').addEventListener('click', () => {
-//     window.location.href = `https://projects.mesure.x10.mx/countdown/generator/?caption=${caption}&showcaption=${showcaption}&endtime=${endtime}&endmessage=${endmessage}&icon=${icon}&title=${title}`;
-// })
+document.getElementById('settings').addEventListener('click', () => {
+    window.location.href = `https://projects.mesure.x10.mx/countdown/generator/?caption=${caption}&showcaption=${showcaption}&endtime=${endtime}&endmessage=${endmessage}&icon=${icon}&title=${title}`;
+})
